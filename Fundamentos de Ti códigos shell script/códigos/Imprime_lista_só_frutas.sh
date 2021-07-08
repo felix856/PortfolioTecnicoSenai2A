03c774f7a4b96cfeba0a5b728cbc845beed465f6
felix856 <82882273+felix856@users.noreply.github.com>
2021-07-07 22:39:58 -0300

    Update Imprime_lista_só_frutas.sh

--- Fundamentos de Ti códigos shell script/códigos/Imprime_lista_só_frutas.sh
+++ Fundamentos de Ti códigos shell script/códigos/Imprime_lista_só_frutas.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 valor=$( cut -c3-23 lista_de_compras.sh )
 echo $valor
+______________________________
+Arquivo lista_de_compras.sh 
+
+1. Laranja.. 2,00
+2. Maça..... 3,50
+3. Açai......... 7,00
+4. Tangerina... 4,00 
+5. Abacaxi...... 4,00
+6. Caju......... 6,00
+7. Mamão........ 2,30
+8. Pitanga...... 4,30
+ 9. Romã........ 8,50
+10. Acerola...... 6,30 
+11. Graviola..... 3,80
