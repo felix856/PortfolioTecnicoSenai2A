46b3b37f3a84295991f6f19ee92cb1c85f201434
felix856 <82882273+felix856@users.noreply.github.com>
2021-07-07 22:33:20 -0300

    Create Imprime_lista_e_ordem_alfabetica.sh

---
+++ It Basics shell script codes/Imprime_lista_e_ordem_alfabetica.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+valor=$( cut -c3-30 lista_de_compras.sh | sort )
+
+echo $valor
+
+
+-----------------------------
+
+Arquivo lista_de_compras.sh 
+
+1. Laranja.. 2,00
+2. Maça..... 3,50
+3. Açai......... 7,00
+4. Tangerina... 4,00 
+5. Abacaxi...... 4,00
+6. Caju......... 6,00
+7. Mamão........ 2,30
+8. Pitanga...... 4,30
+ 9. Romã........ 8,50
+10. Acerola...... 6,30 
+11. Graviola..... 3,80
