4709e0a0776a88879a3fceddd7515e28897124de
Félix Francisco <82882273+felix856@users.noreply.github.com>
2022-05-01 10:39:28 -0300

    Create Plataforma_de_Serviços.java

---
+++ Cod_Java/Plataforma_de_Serviços.java
@@ -0,0 +1,3 @@
+##Dev:Félix Francisco
+//Na matéria de modelagem de dados criamos a1 estrutura de códigos com classes,atributos,e métodos,que devem ser criados conforme pede o exercicio
+//Deve ser executável sem a necessidade de haver métodos realizando ações e criando objetos
